090a0cdf42662b688e84038957b12b67c539db1f
wassim <wbouricha5@gmail.com>
2022-01-04 16:22:02 +0100

    go to the same server as exercice1

--- csv.js
+++ csv.js
@@ -3,9 +3,9 @@ const http = require("http");
 const host = 'localhost';
 const port = 8000;
 
-const requestListener = function (req, res) {};
-
-const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
-});
+const requestListener = function (req, res) {
+    res.setHeader("Content-Type", "text/csv");
+    res.setHeader("Content-Disposition", "attachment;filename=oceanpals.csv");
+    res.writeHead(200);
+    res.end(`id,name,email\n1,Sammy Shark,shark@ocean.com`);
+};
